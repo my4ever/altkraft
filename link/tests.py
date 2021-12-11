8408cbb0fa75595d5016968b71cc062ceaffa9bf
Ruslan <ruslan87yakimov@gmail.com>
2021-12-11 13:18:03 +0300

    testing model done

--- link/tests.py
+++ link/tests.py
@@ -6,6 +6,19 @@ from .views import add_url, generate_code, show_url
 class LinkModelTest(TestCase):
     @classmethod
     def setUpClass(cls):
+        super().setUpClass()
         cls.url = 'https://www.youtube.com/watch?v=kJQP7kiw5Fk&t=2s&ab_channel=LuisFonsiVEVO'
         cls.code = 'GGGG8888'
+        cls.link_instance = Link.objects.create(url=cls.url, code=cls.code)
 
+    def test_model_link_return_value(self):
+        """
+        Testing Links model return value.
+        """
+        self.link = str(self.link_instance)
+        correct_response = f'{self.url} - {self.code}'
+
+        self.assertEqual(self.link, correct_response)
+
+    def tearDown(self):
+        self.link_instance.delete()
